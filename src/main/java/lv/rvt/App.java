dd7e9df5aea89f55891d17c082c6360491c61fe5
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-09-06 11:20:59 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,25 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.println("Ievadit vardu: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadiet uzvardu: ");
+        String surname = scanner.nextLine();
+
+        System.out.println("Ievadiet grupu: ");
+        String group = scanner.nextLine();
+
+
+        System.out.println("Students: " + " " + name  + " " +  surname  + " " +  group);
 
     }
 
