e447671171987ee13a51b1f454ef31dd3bb13fcd
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-09-25 07:16:12 +0000

    Pārtaisiju programmu ar specialiem simboliem

--- src/main/java/lv/rvt/GiftTax.java
+++ src/main/java/lv/rvt/GiftTax.java
@@ -14,13 +14,13 @@ public class GiftTax
 
         if (number < 5000) {
             System.out.println("No tax!");
-        } else if (number < 25000) {
+        } else if (number >= 5000 && number < 25000) {
             System.out.println("Tax: " + (100 + (number-5000) * 0.08));
-        } else if (number < 55000) {
+        } else if (number >= 25000 && number < 55000) {
             System.out.println("Tax: " + (1700 + (number-25000) * 0.10));
-        } else if (number < 200000) {
+        } else if (number >= 55000 && number < 200000) {
             System.out.println("Tax: " + (4700 + (number-55000) * 0.12));
-        } else if (number < 1000000) {
+        } else if (number >= 200000 && number <= 1000000) {
             System.out.println("Tax: " + (22100 + (number-200000) * 0.15));
         } else {
             System.out.println("Tax: " + (142100 + (number-200000) * 0.17));
