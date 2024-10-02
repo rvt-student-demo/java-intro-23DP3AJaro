ecd50385fe5855de43233ad28d8456b1a78edae4
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-10-02 06:55:45 +0000

    RepeatingBreakingAndRemembering: part 5

--- src/main/java/lv/rvt/RepeatingBreakingAndRemembering.java
+++ src/main/java/lv/rvt/RepeatingBreakingAndRemembering.java
@@ -11,6 +11,8 @@ public class RepeatingBreakingAndRemembering
         System.out.println("Give numbers:");
         int sum = 0;
         int count = 0;
+        int even = 0;
+        int odd = 0;
 
 
         while (true) { 
@@ -18,19 +20,27 @@ public class RepeatingBreakingAndRemembering
             if (UserInput == -1) {
                 System.out.println("Thx! Bye!");
                 break;
-            } else {
+            } else if (UserInput % 2 == 0)  {
                 sum = sum + UserInput;
                 count = count + 1;
+                even = even + 1;
+                continue;
+            } else if (UserInput % 2 != 0) {
+                sum = sum + UserInput;
+                count = count + 1;
+                odd = odd + 1;
                 continue;
             }
                 
 
 
         }
-        double average = sum / count;
+        double average = (double)sum / count;
         System.out.println("Sum: " + sum);
         System.out.println("Numbers: " + count);
         System.out.println("Average: " + average);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
     }
 
 }
