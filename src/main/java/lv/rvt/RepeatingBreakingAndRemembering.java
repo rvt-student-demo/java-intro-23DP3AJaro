18b1b6f861af3dc2274e454b98030c77faf02009
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-10-02 06:45:16 +0000

    RepeatingBreakingAndRemembering: Part2

--- src/main/java/lv/rvt/RepeatingBreakingAndRemembering.java
+++ src/main/java/lv/rvt/RepeatingBreakingAndRemembering.java
@@ -9,20 +9,23 @@ public class RepeatingBreakingAndRemembering
         
         Scanner scanner = new Scanner(System.in); 
         System.out.println("Give numbers:");
-        
+        int sum = 0;
+
+
         while (true) { 
             int UserInput = Integer.valueOf(scanner.nextLine());
             if (UserInput == -1) {
                 System.out.println("Thx! Bye!");
                 break;
             } else {
+                sum = sum + UserInput;
                 continue;
             }
                 
 
 
         }
-
+        System.out.println("Sum: " + sum);
     }
 
 }
