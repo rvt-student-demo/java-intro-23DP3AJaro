94de53e1e27a6b4ce9d1510ba793a6780f3b868f
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-10-09 07:47:34 +0000

    Izveidoju šablonu Stars uzdevumam

---
+++ src/main/java/lv/rvt/Stars.java
@@ -0,0 +1,19 @@
+package lv.rvt;
+
+import java.util.*;
+
+public class Stars {
+    public static void printStars(int number) {
+        // you can print one star with the command
+        // System.out.print("*");
+        // call the print command n times
+        // in the end print a line break with the comand
+        // System.out.println("");
+    }
+    
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
+    }
+}
