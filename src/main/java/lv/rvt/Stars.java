ef51c7e4e32b1ad79b57c473d84b4783dfd55406
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-10-11 05:42:54 +0000

    Stars: part 1

--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -1,19 +1,19 @@
 package lv.rvt;
 
-import java.util.*;
+public class Stars 
+{
 
-public class Stars {
     public static void printStars(int number) {
-        // you can print one star with the command
-        // System.out.print("*");
-        // call the print command n times
-        // in the end print a line break with the comand
-        // System.out.println("");
+        for (int i = 0; i <= number; i = i + 1) {
+            System.out.print("*");
+        }
+        System.out.println();
+
     }
-    
     public static void main(String[] args) {
         printStars(5);
         printStars(3);
         printStars(9);
     }
+
 }
