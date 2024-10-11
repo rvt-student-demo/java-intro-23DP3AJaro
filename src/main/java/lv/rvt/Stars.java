07406803a4d0259a0b0f6f24c44d8926c5061e2f
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-10-11 05:51:02 +0000

    Stars: Part 2

--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -4,16 +4,22 @@ public class Stars
 {
 
     public static void printStars(int number) {
-        for (int i = 0; i <= number; i = i + 1) {
+        for (int i = 0; i < number; i = i + 1) {
             System.out.print("*");
         }
         System.out.println();
 
     }
+
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i = i + 1) {
+            printStars(size);
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        printStars(5);
-        printStars(3);
-        printStars(9);
+        printSquare(4);
+        
     }
 
 }
