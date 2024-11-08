60ee6ab1bfd47c70368bcb57d0edebe5194bcf04
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-11-08 07:43:38 +0000

    Izpildīju product uzdevumu

---
+++ src/main/java/lv/rvt/Product.java
@@ -0,0 +1,18 @@
+package lv.rvt;
+
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+
+    public Product (String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void printProduct(){
+        System.out.println(name+","+" price "+price+", "+quantity+" pcs");
+    }
+}
