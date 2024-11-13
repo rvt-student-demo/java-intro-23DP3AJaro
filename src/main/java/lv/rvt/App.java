e50375bbda3c421d7548a4faf28b5ea6ef5da4cc
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-11-13 07:52:48 +0000

    Statistics

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,15 +5,26 @@ import java.util.*;
 public class App 
 {
     public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(100);
-
-        counter.printValue();
-
-        counter.reset();
-        counter.printValue();
-
-        counter.decrement();
-        counter.printValue();
+        Statistics statistics = new Statistics();
+        Scanner scanner = new Scanner(System.in);
+        int even = 0;
+        int odd = 0;
+        System.out.println("Enter numbers: ");
+        while (true) {
+            int UserInput = scanner.nextInt();
+            if (UserInput == -1) {
+                break;
+            } else if (UserInput % 2 == 0) {
+                even = even + UserInput;
+                statistics.addNumber(UserInput);
+            } else if (UserInput % 2 != 0) {
+                odd = odd + UserInput;
+                statistics.addNumber(UserInput);
+            }
+        }
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: "+even);
+        System.out.println("Sum of odd numbers: "+odd);
     }
 
 }
