c3e9c0cb8af9491672650c22effb2b8a63ab8092
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-11-13 06:44:44 +0000

    Decreasing counter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,11 +4,16 @@ import java.util.*;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        
-        Product banana = new Product("Banana", 1.1, 13);
-        banana.printProduct();
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(100);
+
+        counter.printValue();
+
+        counter.reset();
+        counter.printValue();
+
+        counter.decrement();
+        counter.printValue();
     }
 
 }
