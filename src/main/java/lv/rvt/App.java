88a8ad31524077ed3251d95aac0fdb72187aead6
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-11-22 07:17:50 +0000

    One minute

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,39 +5,18 @@ import java.util.*;
 public class App 
 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Book> books = new ArrayList<>();
+        Timer timer = new Timer();
 
         while (true) {
-            System.out.print("Title: ");
-            String Name = scanner.nextLine();
-            if (Name.isEmpty()) {
-                break;
-            }
-
-            System.out.print("Pages: ");
-            int Pages = scanner.nextInt();
-            scanner.nextLine();
-
-            System.out.print("Publication year: ");
-            int Year = scanner.nextInt();
-            scanner.nextLine();
-
-            books.add(new Book(Name, Pages, Year));
-        }
-
-        System.out.print("What information will be printed? ");
-        String choice = scanner.nextLine();
-
-        if (choice.equals("everything")) {
-            for (Book book : books) {
-                System.out.println(book);
-            }
-        } else if (choice.equals("name")) {
-                for (Book book : books) {
-                    System.out.println(book.getName());
-                }
+            System.out.println(timer);
+            timer.advance();
+        
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+            
             }
         }
+    }
 }
 
