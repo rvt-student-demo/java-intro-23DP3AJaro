faededd41b6e29db320998d95173fd843e3864c0
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-12-06 08:45:35 +0000

    HealthStation 3 part

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,21 +5,23 @@ import java.util.*;
 public class App 
 {
     public static void main(String[] args) {
-    HealthStation childrensHospital = new HealthStation();
+        HealthStation childrensHospital = new HealthStation();
 
-    Person ethan = new Person("Ethan", 1, 110, 7);
-    Person peter = new Person("Peter", 33, 176, 85);
-
-    System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-    System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-
-    childrensHospital.feed(ethan);
-    childrensHospital.feed(ethan);
-    childrensHospital.feed(ethan);
-
-    System.out.println("");
-
-    System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-    System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-}
+        Person ethan = new Person("Ethan", 1, 110, 7);
+        Person peter = new Person("Peter", 33, 176, 85);
+    
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+    
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(peter);
+    
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+    
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(ethan);
+        childrensHospital.weigh(ethan);
+    
+        System.out.println("weighings performed: " + childrensHospital.weighings());
+    }
 }
--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -1,12 +1,19 @@
 package lv.rvt;
 
 public class HealthStation {
+    int count = 0;
 
     public int weigh(Person person) {
+        count = count + 1;
         return person.getWeight();
     }
 
     public void feed(Person person) {
         person.setWeight(person.getWeight() + 1);
+    }
+
+    public int weighings() {
+        
+        return count;
     }
 }
