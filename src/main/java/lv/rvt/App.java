aa8b51cd1f9bedc80f2c2ac84b092843056a6b4e
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-12-06 08:36:00 +0000

    HealthStation 1 part

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,13 +5,14 @@ import java.util.*;
 public class App 
 {
     public static void main(String[] args) {
-        Product tapeMeasure = new Product("Tape measure");
-        Product plaster = new Product("Plaster", "home improvement section");
-        Product tyre = new Product("Tyre", 5);
-        
-        System.out.println(tapeMeasure);
-        System.out.println(plaster);
-        System.out.println(tyre);
+        // example main program for the first section of the exercise
+    
+        HealthStation childrensHospital = new HealthStation();
+    
+        Person ethan = new Person("Ethan", 1, 110, 7);
+        Person peter = new Person("Peter", 33, 176, 85);
+    
+        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
     }
 }
-
