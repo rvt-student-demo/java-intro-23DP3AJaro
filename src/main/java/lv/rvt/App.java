6485c3cfa24c084a8493e42d3278b014b1e50a37
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2024-12-06 08:41:32 +0000

    Health station 2 part

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,14 +5,21 @@ import java.util.*;
 public class App 
 {
     public static void main(String[] args) {
-        // example main program for the first section of the exercise
-    
-        HealthStation childrensHospital = new HealthStation();
-    
-        Person ethan = new Person("Ethan", 1, 110, 7);
-        Person peter = new Person("Peter", 33, 176, 85);
-    
-        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-    }
+    HealthStation childrensHospital = new HealthStation();
+
+    Person ethan = new Person("Ethan", 1, 110, 7);
+    Person peter = new Person("Peter", 33, 176, 85);
+
+    System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+    System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+
+    childrensHospital.feed(ethan);
+    childrensHospital.feed(ethan);
+    childrensHospital.feed(ethan);
+
+    System.out.println("");
+
+    System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+    System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+}
 }
--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -5,4 +5,8 @@ public class HealthStation {
     public int weigh(Person person) {
         return person.getWeight();
     }
+
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
 }
