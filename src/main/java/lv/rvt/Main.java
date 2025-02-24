87798c8340324228ea04c63eff36dfae9b80fb47
23DP3AJaro <166131667+23DP3AJaro@users.noreply.github.com>
2025-02-24 14:29:48 +0000

    PersonManager (Part 3)

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -7,9 +7,7 @@ public class Main
 {
     public static void main(String[] args) throws Exception {
 
-        Person newPerson = new Person("Bennet", 27, 69, 185, "Los Angeles");
-
-        PersonManager.addPerson(newPerson);
+        PersonManager.printPersonTable();
     }   
     
 }
--- src/main/java/lv/rvt/PersonManager.java
+++ src/main/java/lv/rvt/PersonManager.java
@@ -43,4 +43,30 @@ public class PersonManager {
         writer.write(person.getName() + ", " + person.getAge() + ", " + person.getWeight() + ", " + person.getHeight() + ", " + person.getAddress());
         writer.close();
     }
-}
+
+    public static void printPersonTable() throws Exception {
+        BufferedReader reader = Helper.getReader("persons.csv");
+        String line;
+
+        reader.readLine();
+        System.out.println("-------------------------------------------------------------------");
+        System.out.printf("%-20s %-5s %-5s %-5s %-20s %-1s%n", "| Name", "| Age", "| Weight", "| Height", "| Address ", "|");
+        System.out.println("-------------------------------------------------------------------");
+
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(", ");
+            
+            String name; 
+            int age;
+            int weight;
+            int height;
+            String address;
+            name = parts[0];
+            age = Integer.valueOf(parts[1]);
+            weight = Integer.valueOf(parts[2]);
+            height = Integer.valueOf(parts[3]);
+            address = parts[4];
+            System.out.printf("%-20s %-5s %-5s %-5s %-20s %-1s%n", "| " + name, "| " + age, "| " + weight, "| " + height, "| " + address, "|"); 
+    }
+        }
+    }
